26ed83a83db031b463497306bf3d29b25156c6d8
nastya shishkina <ana.koze@yandex.ru>
2024-02-13 20:12:29 +0400

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,6 @@ DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        #'USER': 'postgres',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
-        #'PASSWORD': ''
     }
 }
 
